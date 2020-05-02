bb575040910211bfcedd9ea1a8bc0a58dad2b5cd
ravibhut <ravi.bhut@rapidops.com>
2020-05-03 00:24:07 +0530

    simple submit form using Reactive Forms 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormGroupName, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Forms';
+export class AppComponent implements OnInit{
+
+  isEdit: boolean;
+  constructor(){
+
+  }
+
+  ngOnInit(){
+    
+  }
+  // topics = [
+  //   { key: 0, value: 'Angular'},
+  //   { key: 1, value: 'Vue'},
+  //   { key: 2, value: 'React'}
+  // ];
+  topics = [ 'Angular','Vue','React' ];
+  registrationForm = new FormGroup({
+    userName: new FormControl(''),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    topic: new FormControl(''),
+    timePreferemce: new FormControl(''),
+    cheak1: new FormControl(''),
+  });
+    
+  loadApiData() {
+    this.registrationForm.setValue({
+      userName:'ravi',
+      email: 'ravi@gmail.com',
+      phone: '0000000000',
+      topic: 'Angular',
+      timePreferemce: 'morning',
+      cheak1: true,
+    });  
+  }
+
+  edit(){
+    const user  = JSON.parse(localStorage.getItem('user'));
+    console.log('user', user);
+    this.registrationForm.setValue({
+      userName: user.userName,
+      email: user.email,
+      phone: user.phone,
+      topic: user.topic,
+      timePreferemce: user.timePreferemce,
+      cheak1: user.cheak1,
+    });  
+  }
+
+  submit() {
+    console.log('form value', this.registrationForm.value);
+    localStorage.setItem('user', JSON.stringify(this.registrationForm.value));
+    this.registrationForm.reset();
+    this.isEdit = true;
+  }
 }
